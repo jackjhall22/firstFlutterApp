6df0dfcaeabe0c98d03783ec9241d02506f3e1bd
jhal32 <jack.hall@nike.com>
2020-06-08 13:02:09 -0700

    task: create a questions widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
 // void main() {
 //   runApp(MyApp());
 // }
@@ -10,16 +11,16 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex == 0 ? questionIndex + 1 : 1;
+      _questionIndex = _questionIndex == 0 ? _questionIndex + 1 : 1;
     });
     print('Answer Chosen!');
   }
@@ -27,7 +28,7 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(context) {
     var questions = [
-      'What\'s your favorite color',
+      'What\'s your favorite color?',
       'What\'s your favorite animal?',
     ];
     return MaterialApp(
@@ -37,18 +38,18 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(questions[questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
           ],
         ),
