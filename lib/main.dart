9f7f5adb06504b0e603eddf34ea05ef9813b8dfc
jhal32 <jack.hall@nike.com>
2020-06-15 19:41:30 -0500

    add a ternary statement

--- lib/main.dart
+++ lib/main.dart
@@ -17,52 +17,54 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['red', 'blue', 'green', 'purple'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        'dog',
+        'cat',
+        'cow',
+        'rabbit',
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': ['paulo', 'bob', 'jack', 'max'],
+    },
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-      _questionIndex = _questionIndex < 2 ? _questionIndex + 1 : 0;
+      _questionIndex = _questionIndex + 1;
     });
     print('Answer Chosen!');
   }
 
   @override
   Widget build(context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['red', 'blue', 'green', 'purple'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': [
-          'dog',
-          'cat',
-          'cow',
-          'rabbit',
-        ],
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': ['paulo', 'bob', 'jack', 'max'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Text('YOU DID IT!'),
       ),
     );
   }
